0d1870697bcfa6f3f74b566b2522a5c603052505
Manuel <manuel@guerrerocabrera.com>
2024-01-22 22:23:11 +0100

    He creado el header dinámico

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+import { Header } from './src/components/Header/Header';
 import './style.css';
+
+// Seleccionar el elemento HTML donde se pintará la aplicación
+const divApp = document.querySelector("#app");
+
+// Creamos la cabecera con el menú de navegación
+const header = Header();
+
+// Añadimos cabecera la aplicación
+divApp.appendChild(header);
+
+const btTest = document.createElement("button");
+btTest.textContent = "Mostrar menú";
+
+const headerDos = document.querySelector("#app-header");
+
+btTest.addEventListener("click", () => {
+    headerDos.style.marginTop = 0;
+});
+
+divApp.appendChild(btTest);
---
+++ src/components/Header/Header.js
@@ -0,0 +1,54 @@
+import "./Header.css";
+
+const navElements = [
+    {
+        name: "Trivial",
+        path: "#"
+    },
+    {
+        name: "Tres en Raya",
+        path: "#"
+    },
+    {
+        name: "Memory Game",
+        path: "#"
+    }
+];
+
+export const Header = () => {
+
+    // Crear elemento HTML header
+    const header = document.createElement("header");
+    
+    // Le indicamos su id a través del cual le daremos estilos.
+    header.id = "app-header";
+    
+    // Crear menu de navegación
+    const ul = document.createElement("ul");
+    ul.className = "header-menu-nav";
+
+    // Crear elementos de navegación recorriendo el array de elementos
+    for(let i = 0; i < navElements.length; i++){
+
+        // Crear elementos HTML li y a
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+
+        // Dar valores a los enlaces
+        a.src = navElements[i].path;
+        a.textContent = navElements[i].name;
+
+        // Añadir los enlaces a los elementos HTML li
+        li.appendChild(a);
+
+        // Añadir el elemento HTML creado al ul
+        ul.appendChild(li);
+
+    }
+
+    // Añadir el menú de navegación al elemento HTML header
+    header.appendChild(ul);
+
+    // Devolver el header
+    return header;
+}
